e0d6017d95d1db74a00b8a640a4de6bb0199c2f1
Seth Morabito <seth@loomcom.com>
2014-08-16 13:50:47 -0700

    Unifying and cleaning up copyrights

--- src/main/java/com/loomcom/symon/machines/SymonMachine.java
+++ src/main/java/com/loomcom/symon/machines/SymonMachine.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2008-2014 Seth J. Morabito <web@loomcom.com>
- *                         Maik Merten <maikmerten@googlemail.com>
+ * Copyright (c) 2014 Seth J. Morabito <web@loomcom.com>
+ *                    Maik Merten <maikmerten@googlemail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
--- src/main/java/com/loomcom/symon/ui/TraceLog.java
+++ src/main/java/com/loomcom/symon/ui/TraceLog.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2013 Seth J. Morabito <web@loomcom.com>
+ * Copyright (c) 2014 Seth J. Morabito <web@loomcom.com>
+ *                    Maik Merten <maikmerten@googlemail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
